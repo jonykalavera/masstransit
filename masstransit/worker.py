917f34f1419b2664e13732c1c92e799cd3d14b09
Jony Kalavera <mr.jony@gmail.com>
2024-08-05 13:27:22 -0600

    colorful worker

--- masstransit/worker.py
+++ masstransit/worker.py
@@ -8,12 +8,14 @@ from random import choice
 from typing import TYPE_CHECKING
 
 from masstransit.colors import Background, Foreground, Style
-from masstransit.models import Config
 
 if TYPE_CHECKING:
-    from masstransit.models.config import WorkerConfig
+    from masstransit.models.config import Config, WorkerConfig
 
 logger = logging.getLogger(__name__)
+
+
+OUTPUT_TEMPLATE = "{fg}{bg}⑆ {name}:{end} {stdout}"
 
 
 @lru_cache
@@ -24,33 +26,32 @@ def _random_color(*args, **kwargs) -> tuple[str, str]:
 
 
 # Define a function to run a command and stream its output
-def _run_command(name: str, command: list[str], color) -> None:
+def _run_command(name: str, command: list[str]) -> None:
     process = sp.Popen(" ".join(command), stdout=sp.PIPE, stderr=sp.STDOUT, text=True, shell=True)
     # Stream the output line by line
     assert process.stdout, "should get stdout reference"
-    fg, bg = color
+    fg, bg = _random_color(name)
     for stdout in iter(process.stdout.readline, ""):
         if stdout:
-            print(f"{fg}{bg} {name} {Style.CEND} {stdout}", end="")  # noqa: T201
+            output = OUTPUT_TEMPLATE.format(fg=fg, bg=bg, name=name, end=Style.CEND, stdout=stdout)
+            print(output, end="")  # noqa: T201
     if process.stdout:
         process.stdout.close()
     process.wait()
 
 
-def _get_consumers(config: Config, worker: "WorkerConfig") -> tuple[dict[str, list[str]], dict[str, tuple[str, str]]]:
+def _get_consumer_commands(config: "Config", worker: "WorkerConfig") -> dict[str, list[str]]:
     consumers = {}
-    colors = {}
     for consumer in worker.consumers:
         for n in range(1, consumer.number_of_consumers + 1):
             name = f"{worker.display()}:{consumer.display()}-{n:02}"
             command = ["python3", "-m", "masstransit", "consume", consumer.queue]
             consumers[name] = command
-            colors[name] = _random_color(_name=name)
             logger.info("Adding consumer %s", name)
-    return consumers, colors
+    return consumers
 
 
-def start(config: Config, name: str):
+def start(config: "Config", name: str):
     """Start the worker process.
 
     This works as a multi-process worker. Starting a worker will start all consumers in the worker.
@@ -60,12 +61,11 @@ def start(config: Config, name: str):
     if not worker:
         raise ValueError(f"Worker '{name}' not found in config")
     logger.info("Starting worker %s", worker.name)
-    consumers, colors = _get_consumers(config=config, worker=worker)
+    consumers = _get_consumer_commands(config=config, worker=worker)
     # Create a thread for each command
     threads = []
     for _name, cmd in consumers.items():
-        color = colors[_name]
-        thread = threading.Thread(target=_run_command, args=(_name, cmd, color))
+        thread = threading.Thread(target=_run_command, args=(_name, cmd))
         threads.append(thread)
     # Start all threads
     for thread in threads:
